4ebe9bd386ec9d5d717c9f45274dd5c123bfde10
Julio.S <just.julio95@gmail.com>
2022-06-06 18:20:25 -0400

    added login functionality, logout functionality, and session information

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,8 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models')
 
 router.get('/', (req, res) => {
+  console.log(req.session);
+
   Post.findAll({
     // Query configuration
     attributes: ['id', 'post_url', 'title', 'created_at',
@@ -32,5 +34,13 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     })
 });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login')
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,21 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection')
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -14,6 +26,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess))
 
 //turn on routes
 app.use(routes);
